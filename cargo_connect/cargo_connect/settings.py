1c667e6d0bd182e7081fb230b9240167a42daa57
duvindu-embuldeniya <duvindu.personal@gmail.com>
2025-03-03 18:06:43 +0530

    +changes

--- cargo_connect/cargo_connect/settings.py
+++ cargo_connect/cargo_connect/settings.py
@@ -143,5 +143,5 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_POST = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('user')
-EMAIL_HOST_PASSWORD = os.environ.get('app_pass')
+EMAIL_HOST_USER = os.environ.get('email')
+EMAIL_HOST_PASSWORD = os.environ.get('app_password')
